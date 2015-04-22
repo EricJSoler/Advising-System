912dd57e49665ac4615875c3cd1d7f82e0162433
Eric Soler <e.j.soler1@gmail.com>
2015-04-21 22:27:02 -0700

    Naming Convention needs to be changed to something more fitting
    Zaps has been renamed Match.. Only reason Zap was in there in first
    place because I couldn't think of any other names

---
+++ Quarter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sharpAdvising
+{
+    public class Quarter //This is the data type that will be deserialized from The Pre-requisite module
+    {
+        public int qNum;
+        public List<string> courses;
+
+        public Quarter()
+        {
+
+        }
+        public int quarter
+        {
+            get { return qNum; }
+        }
+
+        public List<string> Courses
+        {
+            get { return courses; }
+        }
+
+    }
+}
