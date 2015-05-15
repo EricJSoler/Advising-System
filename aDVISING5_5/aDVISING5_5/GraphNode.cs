590f7f9efd1a7aa01b98e26d61c981dfce9b7a5c
Eric Soler <e.j.soler1@gmail.com>
2015-05-14 18:05:03 -0700

    Graph up on a thursday
    Grap going up on a tthursday

---
+++ aDVISING5_5/aDVISING5_5/GraphNode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sharpAdvising
+{
+   public class GraphNode: CourseGraph 
+    {
+        public GraphNode() { }
+
+        public GraphNode(string dep, string num){
+            department = dep;
+            number = num;
+             
+        }
+
+        public int row;
+        public string department;
+        public string number;
+    }
+}
