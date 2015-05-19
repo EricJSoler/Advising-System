3593e500ad3f99c6476a78a48e04c4b850dbaba5
Eric Soler <e.j.soler1@gmail.com>
2015-05-19 14:20:12 -0700

    graph ready
    Graph thing

--- aDVISING5_5/aDVISING5_5/GraphNode.cs
+++ aDVISING5_5/aDVISING5_5/GraphNode.cs
@@ -3,21 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Xml.Serialization;
+using System.IO;
 
 namespace sharpAdvising
 {
-   public class GraphNode: CourseGraph 
+    public class GraphNode
     {
-        public GraphNode() { }
-
-        public GraphNode(string dep, string num){
-            department = dep;
-            number = num;
-             
+        public GraphNode()
+        {
         }
 
-        public int row;
-        public string department;
-        public string number;
+        public GraphNode(string department, string number, string nodeType)
+        {
+            SqlCommand cmd = new SqlCommand();
+            SqlDataReader reader;
+
+            cmd.CommandText = "dbo.Read_PreReqDep_PreReqN_Ext_GroupID_By_DepID_NumID";
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.Add(new SqlParameter("@DepID",department));
+            cmd.Parameters.Add(new SqlParameter("@NumID", number));
+            cmd.Connection = SQLHANDLER.myConnection2;
+
+            reader = cmd.ExecuteReader();
+            List<String> readDep = new List<String>();
+            List<String> readNum = new List<String>();
+            List<String> ext = new List<String>();
+            List<String> group = new List<String>();
+            while (reader.Read()) ///Pull in the table of pre-requisite for the course
+            {
+                readDep.Add(reader.GetValue(0).ToString());
+                readNum.Add(reader.GetValue(1).ToString());
+                ext.Add(reader.GetValue(2).ToString());
+                group.Add(reader.GetValue(3).ToString());
+            }
+
+            int i = 0;
+            foreach (String element in readDep)
+            {
+                Console.WriteLine(readDep[i]);
+                Console.WriteLine(readNum[i]);
+                Console.WriteLine(ext[i]);
+                Console.WriteLine(group[i]);
+                Console.WriteLine("------------");
+                i++;
+            }
+
+            reader.Close();
+        }
+
+
+        List<GraphNode> children;
+        int inDegree;
+        string department;
+        string number;
+        string nodeType;
     }
 }
--- aDVISING5_5/aDVISING5_5/PreReq.cs
+++ aDVISING5_5/aDVISING5_5/PreReq.cs
@@ -45,9 +45,17 @@ namespace sharpAdvising
 
             reader.Close();
 
-            graph = new CourseGraph();
-            graph.addNode("CS", "233");
-            Console.WriteLine("we here");
+            foreach(Subject element in subjectRequirements)
+            {
+                foreach(Course ele in element.coursesReq)
+                {
+                    Graph.insert(ele.departmentID, ele.numberID);
+                }
+            }
+
+            Console.WriteLine("Graph has been built");
+            
+            
             
          
             
@@ -65,7 +73,7 @@ namespace sharpAdvising
             return false;
         }
 
-        private CourseGraph graph;
+       
       public static string program;
       public List<Subject> subjectRequirements; ///SubjectRequirements is the list of the major related subjects inside each subject is a Course list containing the  Courses required for each department
     }
