02fefee006d48a35a4a85739301fcee27429841b
Emmanuel Harley <mannyharley20@gmail.com>
2015-05-23 16:17:56 -0700

    Graph build function update
    Small fix in the build function, if the prereq we insert is a duplicate,
    search for the index of that course.

--- aDVISING5_5/aDVISING5_5/Graph.cs
+++ aDVISING5_5/aDVISING5_5/Graph.cs
@@ -12,6 +12,9 @@ namespace sharpAdvising
     /// </summary>
     class Graph
     {
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public Graph()
         {
             allCourses = new Dictionary<String, GraphNode>();
@@ -19,7 +22,10 @@ namespace sharpAdvising
             gridDepth = 1;
         }
 
-
+        /// <summary>
+        /// Checks if the depth is the correct size for all lists
+        /// </summary>
+        /// <param name="depth">The current depth to check against</param>
         public void checkDepth(int depth)
         {
             if (gridDepth < depth) {
@@ -33,24 +39,32 @@ namespace sharpAdvising
             }
         }
 
+        /// <summary>
+        /// Adds a single course to the adjacency matrix
+        /// </summary>
         public void addCourseToGrid()
         {
             courseGrid.Add(new List<List<bool>>());
             for (int i = 0; i < courseGrid.Count; i++) {
-                    courseGrid[i].Add(new List<bool>());
-                if(courseGrid.Count > 1 && i > 0)
+                courseGrid[i].Add(new List<bool>());
+                if (courseGrid.Count > 1 && i > 0)
                     courseGrid[courseGrid.Count - 1].Add(new List<bool>());
             }
 
 
             for (int i = 0; i < courseGrid.Count; i++) {
                 for (int j = 0; j < courseGrid[i].Count; j++) {
-                    while(courseGrid[i][j].Count < gridDepth)
+                    while (courseGrid[i][j].Count < gridDepth)
                         courseGrid[i][j].Add(false);
                 }
             }
         }
 
+        /// <summary>
+        /// Builds the graph with the griven list of required courses
+        /// </summary>
+        /// <param name="prereqRows">List of required courses</param>
+        /// <param name="parentIndex">0 for first (for recursion)</param>
         public void build(List<PrereqRow> prereqRows, int parentIndex)
         {
             int path = 0;
@@ -59,23 +73,30 @@ namespace sharpAdvising
                 if (row.prereqDepartmentID == "MASTER")
                     continue;
 
+                int index = 0;
                 // Add course if not a duplicate
                 try {
                     allCourses.Add(row.prereqDepartmentID + row.prereqNumberID, new GraphNode(row.prereqDepartmentID, row.prereqNumberID));
                     addCourseToGrid();
+                    index = allCourses.Count - 1;
                 }
                 catch (ArgumentException e) {
                     //duplicate...
+                    for (int i = 0; i < allCourses.Count; i++) {
+                        if (allCourses.ElementAt(i).Key == row.prereqDepartmentID + row.prereqNumberID) {
+                            index = i;
+                            break;
+                        }
+                    }
                 }
 
-                int index = allCourses.Count - 1;
                 List<PrereqRow> morePrereqs;
                 morePrereqs = getCoursePrereq(row.prereqDepartmentID, row.prereqNumberID);
                 build(morePrereqs, index);
                 checkDepth(path + 1);
                 if (row.type == "OR") {
-                        courseGrid[parentIndex][index][path++] = true;
-                        checkDepth(path + 1);
+                    courseGrid[parentIndex][index][path++] = true;
+                    checkDepth(path + 1);
                 }
                 else {
                     if (row.groupID.Split('.').Count() < depth.Split('.').Count() ||
@@ -112,6 +133,13 @@ namespace sharpAdvising
                 //}
             }
         }
+
+        /// <summary>
+        /// Gets the list of courses required to take for the desired course
+        /// </summary>
+        /// <param name="departmentID">The courses department ID that you want the prereqs for</param>
+        /// <param name="numberID">The courses number ID that you want the prereqs for</param>
+        /// <returns></returns>
         public List<PrereqRow> getCoursePrereq(String departmentID, String numberID)
         {
             SqlCommand cmd = new SqlCommand();
@@ -164,6 +192,7 @@ namespace sharpAdvising
         /// allCourses is a dictionary of GraphNode using integers as keys. The integer key for each entry in the dictionary will represent what row in the ajacency matrix information about the course can be found
         /// </summary>
         public Dictionary<String, GraphNode> allCourses;
+
         /// <summary>
         /// courseGrid is a 3 dimensional List matrix. Each the rows of the matrix will be used to store information for a course from the allCourses dictionary. Columns Will be used to represent Pre-Requisites for the course in a given row. Width will be used to represent the different possible combinations of taking different courses
         /// i.e. [Course][Pre-Reqs][Combinations] 
@@ -172,17 +201,33 @@ namespace sharpAdvising
         public int gridDepth;
     }
     /// <summary>
-    /// \todo I should really comment this...
+    /// The rows that the db tables return
     /// </summary>
     class PrereqRow
     {
+        /// <summary>
+        /// Represents the department of the MASTER course
+        /// </summary>
         public String departmentID;
+        /// <summary>
+        /// Represents the course number of the MASTER course
+        /// </summary>
         public String numberID;
+        /// <summary>
+        /// Represents the department of the prereq course
+        /// </summary>
         public String prereqDepartmentID;
+        /// <summary>
+        /// Represents the course number of the prereq course
+        /// </summary>
         public String prereqNumberID;
+        /// <summary>
+        /// The type of parent the prereq has (AND or OR)
+        /// </summary>
         public String type;
+        /// <summary>
+        /// The group ID its parent has ex: 1.1.3
+        /// </summary>
         public String groupID;
     }
-
-    
 }
